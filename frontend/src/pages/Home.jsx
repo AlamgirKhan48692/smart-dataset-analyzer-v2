62bbb4e1d166dd238caad43b20384a6865cf177c
Alamgir Khan <alamgirkhan48692@gmail.com>
2026-06-22 16:16:42 +0500

    Fix Home.jsx syntax error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -29,6 +29,7 @@ function Home(){
                     method: "POST",
                     body: formData
                 }
+            )
             );
             const data = await response.json(); 
 
